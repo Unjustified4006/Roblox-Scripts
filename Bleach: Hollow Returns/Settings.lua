2a9bd8ca974c49485d13211c8a4d6c972848b1b3
Bidhya62 <124343364+Bidhya62@users.noreply.github.com>
2023-02-03 03:08:43 -0500

    Create Settings.lua

---
+++ Bleach: Hollow Returns/Settings.lua
@@ -0,0 +1,14 @@
+getgenv().MSSettings = {
+	Enabled = true,
+	maximumPing = 100,
+
+	MoveSpamSettings = {
+		Moves = {"Soul Reaper Special", "Vortex", "Bankai", "Tesseract", "Reitsu Wave", "Celdryx's Hammer", "Shikai", "Cero Blast", "Gamma Quake", "Quincy Arrow", "Sinful Spear", "Gamma Ray Cannon", "Arrancar Cero", "Hero's Sword", "Cero", "Cero Bomb", "Gran Rey Cero", "Getsuga Tensho", "Royal Sword", "Mace of Spheres", "Mugetsu", "NormalSword", "Hado 33", "Venom Breathe", "Seele Schneider", "Multi Shot", "Sinful Flame", "Sinners Burst", "Sphere Ball", "Explosive Shot", "Soren Sokatsui", "Gravity Ball", "Sphere Burst", "Blade Explosion", "Hado 31", "Sphere Shower", "Auto Turret", "Mage Blast", "UpperCut Swing", "Venom Explosion", "Sinners Explosion", "Spread shot", "Sphere Storm", "Homing Arrow", "Ki Burst", "Cero Beam", "Sinful Punch", "Warrior Swing", "Trishot", "Rapid Fire", "Jumping Sidekick", "Reitsu Blast", "Blade Fall", "Moon blade", "Shockwave", "Venom Spit", "Mage Burst", "Warrior Slam", "Fire Star", "Blade Launcher", "Mage normal", "Bala", "Volley", "Attack"}
+	},
+	Cooldowns = { -- wait():0.03309060000083264
+		attackCd = 0.13, -- might be same for this as well
+		targetSwitchCd = 0.09, --0.03, -- any faster will result in some dummies not being hit
+	},
+}
+
+-- limit = 51kb
